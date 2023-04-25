d9029252330460c1e1c71b149545f9062c7e7dc6
azikreed <azikreed@gmail.com>
2023-04-25 11:32:06 +0500

    fix

--- config.js
+++ config.js
@@ -7,10 +7,6 @@ const config = {
   domain: process.env.WEBHOOK_DOMAIN,
   port: process.env.PORT,
   mongodbUri: process.env.MONGODB_URI,
-  admin_name: process.env.ADMIN_NAME,
-  admin_tg_id: process.env.ADMIN_TELEGRAM_ID,
-  admin_username: process.env.ADMIN_USERNAME,
-  admin_password: process.env.ADMIN_PASSWORD,
   group_id: process.env.GROUP_ID
 };
 
